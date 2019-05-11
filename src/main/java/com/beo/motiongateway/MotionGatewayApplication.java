9d050bad68376f587765cc76b508abcd9e88c5af
bennyben54 <bennyben54@gmail.com>
2019-05-11 23:36:00 +0200

    [FEAT](gateway) first implementation of gateway

---
+++ src/main/java/com/beo/motiongateway/MotionGatewayApplication.java
@@ -0,0 +1,40 @@
+package com.beo.motiongateway;
+
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootApplication
+public class MotionGatewayApplication {
+
+    private final static List<String> args = new ArrayList<>();
+
+    public static void main(String[] args) {
+        MotionGatewayApplication.args.addAll(Arrays.asList(args));
+        SpringApplication.run(MotionGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator getLocatorBean(RouteLocatorBuilder builder) {
+        String cameraAddress;
+        if (args.isEmpty() || Strings.isEmpty(args.get(0))) {
+            cameraAddress = "localhost";
+        } else {
+            cameraAddress = args.get(0);
+        }
+        return builder.routes()
+                .route("route_cam1", r -> r.path("/cam1")
+                        .uri(String.format("http://%s:8081", cameraAddress)))
+                .route("route_cam2", r -> r.path("/cam2")
+                        .uri(String.format("http://%s:8082", cameraAddress)))
+                .build();
+    }
+
+}
